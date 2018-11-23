c286f122d39e13691cee00a1e5de25b3e18147b9
chenchujie <zjchenchujie@126.com>
2018-11-23 09:10:47 +0800

    first try. compilable

---
+++ src/caffeine/common.hpp
@@ -0,0 +1,25 @@
+//
+// Created by chujie on 18-11-22.
+//
+
+#ifndef BLACK_CAFFE_COMMON_H
+#define BLACK_CAFFE_COMMON_H
+
+#include <iostream>
+
+#include <boost/shared_ptr.hpp>
+
+#include "driver_types.h"
+
+namespace caffeine {
+    using boost::shared_ptr;
+}
+
+static std::ostream nullout(0);
+
+#define LOG_IF(condition) \
+    ((condition) == cudaSuccess) ? nullout : std::cout
+#define CUDA_CHECK(condition) \
+    LOG_IF(condition) << "Check failed: " #condition " "
+
+#endif //BLACK_CAFFE_COMMON_H
---
+++ src/caffeine/syncedmem.hpp
@@ -0,0 +1,32 @@
+//
+// Created by chujie on 18-11-22.
+//
+
+#ifndef BLACK_CAFFE_SYNCEDMEN_HPP
+#define BLACK_CAFFE_SYNCEDMEN_HPP
+
+namespace caffeine {
+
+class SyncedMemory {
+public:
+    SyncedMemory()
+        :cpu_ptr_(NULL), gpu_ptr_(NULL), size_(0), head_(SYNCED){};
+    SyncedMemory(size_t size)
+        :cpu_ptr_(NULL), gpu_ptr_(NULL), size_(size), head_(SYNCED){};
+    ~SyncedMemory();
+    const void* cpu_data();
+    const void* gpu_data();
+    void* mutable_cpu_data();
+    void* mutable_gpu_data();
+private:
+    void to_cpu();
+    void to_gpu();
+    void *cpu_ptr_;
+    void *gpu_ptr_;
+    size_t size_;
+    enum SyncedHead { UNINITIATED, HEAD_AT_CPU, HEAD_AT_GPU, SYNCED };
+    SyncedHead head_;
+};    // class SyncedMemory
+} // namespace caffeine
+
+#endif //BLACK_CAFFE_SYNCEDMEN_HPP
