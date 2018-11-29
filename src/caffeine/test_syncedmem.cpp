94e715f0d96de7f3bbc1472917e8468e46b4dc23
chenchujie <zjchenchujie@126.com>
2018-11-29 14:08:58 +0800

    add more test.

--- src/caffeine/test_syncedmem.cpp
+++ src/caffeine/test_syncedmem.cpp
@@ -1,5 +1,9 @@
 #include "gtest/gtest.h"
 #include "caffeine/syncedmem.hpp"
+#include "caffeine/common.hpp"
+
+#include <cuda_runtime.h>
+
 
 namespace caffeine {
 
@@ -14,6 +18,30 @@ TEST_F(SyncedMemoryTest, TestAllocation){
     SyncedMemory mem(10);
     EXPECT_NE(mem.cpu_data(), (void*)NULL);
     EXPECT_NE(mem.gpu_data(), (void*)NULL);
+}
+
+TEST_F(SyncedMemoryTest, TestCPUWrite){
+    SyncedMemory mem(10);
+    void *cpu_data = mem.mutable_cpu_data();
+    EXPECT_EQ(SyncedMemory::HEAD_AT_CPU, mem.head());
+    memset(cpu_data, 1, mem.size());
+    for(int i=0; i<mem.size(); i++)
+    {
+        EXPECT_EQ(1, ((char*)cpu_data)[i]);
+    }
+    const void *gpu_data = mem.gpu_data();
+    EXPECT_EQ(SyncedMemory::SYNCED, mem.head());
+}
+
+TEST_F(SyncedMemoryTest, TestGPUWrite){
+    SyncedMemory mem(10);
+    void *gpu_data = mem.mutable_gpu_data();
+    EXPECT_EQ(SyncedMemory::HEAD_AT_GPU, mem.head());
+    CUDA_CHECK(cudaMemset(gpu_data, 1, mem.size()));
+    const void* cpu_data = mem.cpu_data();
+    for(int i=0; i<mem.size(); i++){
+        EXPECT_EQ(((char*)cpu_data)[i], 1);
+    }
 }
 
 }
