33ed21ed3769905cd9e8a80e39f451548bfcc2e7
chenchujie <zjchenchujie@126.com>
2019-01-17 14:57:50 +0800

    layer factory

---
+++ src/caffe/layer_factory.hpp
@@ -0,0 +1,41 @@
+//
+// Created by chujie on 1/17/19.
+//
+
+#ifndef BLACK_CAFFE_LAYER_FACTORY_HPP
+#define BLACK_CAFFE_LAYER_FACTORY_HPP
+
+#include "caffe/layer.hpp"
+#include "caffe/vision_layers.hpp"
+#include "proto/layer_param.pb.h"
+
+namespace caffe {
+
+    template <typename Dtype>
+    Layer<Dtype>* GetLayer(const LayerParameter &param){
+        const std::string &type = param.type();
+        if(type == "conv"){
+            return new ConvolutionLayer<Dtype>(param);
+        }else if(type == "dropout"){
+            return new DropoutLayer<Dtype>(param);
+        }else if(type == "im2col"){
+            return new Im2colLayer<Dtype>(param);
+        }else if(type == "innerproduct"){
+            return new InnerProductLayer<Dtype>(param);
+        }else if(type == "lrn"){
+            return new LRNLayer<Dtype>(param);
+        }else if(type == "padding"){
+            return new PaddingLayer<Dtype>(param);
+        }else if(type == "pool"){
+            return new PoolingLayer<Dtype>(param);
+        }else if(type == "relu"){
+            return new ReLULayer(param);
+        }else {
+            LOG(FATAL) << "Unkonwn layer name: " << type;
+        }
+        return (Layer<Dtype> *)(NULL);
+    }
+} // namespace caffe
+
+
+#endif //BLACK_CAFFE_LAYER_FACTORY_HPP
